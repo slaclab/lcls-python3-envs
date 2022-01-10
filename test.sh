4ac7b946a99c33a4853d2c1efe31097d9110cbcd
jbellister-slac <jesseb@slac.stanford.edu>
2022-01-10 11:33:46 -0800

    Enable the testing part of the workflow using the test file with package imports from @jacquelinegarrahan

---
+++ test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+python - <<EOF
+import socks
+import parso
+import matplotlib
+import PIL
+import tornado
+import pyqtgraph
+import pydm
+import qtpy
+import statsmodels
+import prometheus_client
+import tensorflow
+import yaml
+import keras
+import pandas
+import bokeh
+import epics
+import numpy
+import theano
+import pasta
+import p4p
+import sklearn
+import pytao
+import treelib
+import pytest
+EOF
