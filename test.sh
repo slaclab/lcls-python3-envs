3960fe41b54d0c4bbc484bc7264812c470157aa0
jbellister-slac <89539359+jbellister-slac@users.noreply.github.com>
2023-10-23 13:53:51 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,6 +2,10 @@
 conda remove --force --offline --yes jax
 conda remove --force --offline --yes jaxlib
 
+pip install jax==0.4.16
+pip install jaxlib==0.4.19
+pip install jaxtyping==0.2.21
+
 git clone https://github.com/ChristopherMayes/Xopt.git
 cd Xopt
 pytest
