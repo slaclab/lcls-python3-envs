0cb7eb087e2e595821d19c080a5f192d779f86a3
jbellister-slac <jesseb@slac.stanford.edu>
2023-11-02 19:58:39 -0700

    Fix issue with test script

--- test.sh
+++ test.sh
@@ -3,11 +3,9 @@
 # Temporary fix for a jax incompatibility, can be removed once that is resolved
 conda remove --force --offline --yes jax
 conda remove --force --offline --yes jaxlib
-pip install jax==0.4.16
-pip install jaxlib==0.4.19
-pip install jaxtyping==0.2.21
+pip install jax
 
 # All tests to be run to verify the environment built correctly and will work as expected at run time
 git clone https://github.com/ChristopherMayes/Xopt.git
-cd Xopt
+pushd Xopt
 pytest
