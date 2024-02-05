fd266a20d123674af92f700ed352befa6d94e7d0
jbellister-slac <jesseb@slac.stanford.edu>
2024-02-05 08:19:02 -0800

    See if the jax/tensorflow-probability/botorch issue has resolved

--- test.sh
+++ test.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Temporary fix for a jax incompatibility, can be removed once that is resolved
-conda remove --force --offline --yes jax
-conda remove --force --offline --yes jaxlib
-pip install jax
-
 # All tests to be run to verify the environment built correctly and will work as expected at run time
 git clone --depth=1 https://github.com/ChristopherMayes/Xopt.git
 pushd Xopt
