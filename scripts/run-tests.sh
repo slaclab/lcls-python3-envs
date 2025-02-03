2faa7fd8759e49d213ea9191664def4074732508
jbellister-slac <jesseb@slac.stanford.edu>
2025-02-03 15:12:17 -0800

    Update files to point to new locations for xopt and badger

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # All tests to be run to verify the environment built correctly and will work as expected at run time
-git clone --depth=1 https://github.com/ChristopherMayes/Xopt.git
+git clone --depth=1 https://github.com/xopt-org/Xopt.git
 pushd Xopt
 pytest
 popd
